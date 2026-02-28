5e082fb806c495df18f5d70f495f9d0abc124a9b
chathuniimesha <chathuniimesha3444@gmail.com>
2026-02-28 18:53:31 +0530

    Fix Testresult constructor and getter methods

---
+++ src/model/Testresult.java
@@ -0,0 +1,39 @@
+package model;
+
+
+public  class Testresult{
+    private int size;
+    private long sortingexecutiontime;
+    private long searchingexecutetime;
+     private int searchingindex;
+
+    public Testresult(int size ,long sortingexecutiontime,long searchingexecutetime, int searchingindex){
+        this.size=size;
+        this.searchingexecutetime=searchingexecutetime;
+        this.sortingexecutiontime=sortingexecutiontime;
+        this.searchingindex=searchingindex;
+
+    }
+    public int getsize(){
+        return size;
+    }
+    public long getsortingexecutiontime(){
+        return sortingexecutiontime;
+    }
+    public long  getsearchingexecutetime(){
+        return searchingexecutetime;
+    }
+     public int getsearchingindex(){
+        return searchingindex;
+    }
+
+
+
+    
+}
+    
+        
+    
+
+
+
--- src/models/Testresult.java
+++
@@ -1,1 +0,0 @@
-package moduleN3.models;
